4352ed2732b470c187d141965c400cc5c8e3a902
Adam Price <komidore64@gmail.com>
2018-01-19 15:21:35 -0500

    config show - command for bad file

--- test/temjin/config_command/show_command_test.rb
+++ test/temjin/config_command/show_command_test.rb
@@ -4,13 +4,14 @@ module Temjin
       include CaptureStdout
 
       def setup
-        @test_config_path_proper ||= File.join(File.expand_path(Temjin::Test::HOME_DIR), "files", "configs", "proper_config.yml")
+        @test_config_path_proper ||= File.join(File.expand_path(Temjin::Test::HOME_DIR), 'files', 'configs', 'proper_config.yml')
 
-        @show_command = ConfigCommand::ShowCommand.new("")
+        @show_command = ConfigCommand::ShowCommand.new('')
       end
 
       def teardown
         @show_command = nil
+        ConfigCommand.instance_variable_set(:@config_file_path, nil)
       end
 
       def test_show_with_existing_config_file
@@ -24,10 +25,20 @@ module Temjin
           @show_command.execute
         end
 
-        assert_equal expected_output, actual_output
+        assert_equal(expected_output, actual_output)
       end
 
-      def test_show_without_existing_config_file; end
+      def test_show_without_existing_config_file
+        ConfigCommand.instance_variable_set(:@config_file_path, File.join(File.expand_path(Temjin::Test::HOME_DIR), 'path', 'that', 'does', 'not', 'exist.yml'))
+
+        expected_output = "Configuration file not found. Please run `temjin config init`.\n"
+
+        actual_output = capture_stdout do
+          @show_command.execute
+        end
+
+        assert_equal(expected_output, actual_output)
+      end
     end
   end
 end
