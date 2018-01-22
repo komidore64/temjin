aa5554e347c1b6dbd56bfef7c08be5a47e7e11aa
Adam Price <komidore64@gmail.com>
2018-01-22 15:25:20 -0500

    refactor tests to call #run rather than #execute to parse params/options

--- test/temjin/config_command/init_command_test.rb
+++ test/temjin/config_command/init_command_test.rb
@@ -7,7 +7,7 @@ module Temjin
         def setup
           @tempfile = Tempfile.new('config.yml')
           @test_config_path = @tempfile.path
-          @init_command = Temjin::ConfigCommand::InitCommand.new('')
+          @init_command = Temjin::ConfigCommand::InitCommand.new('temjin config init')
         end
 
         def teardown
@@ -16,24 +16,25 @@ module Temjin
           @tempfile.close!
         end
 
-        def test_execute
+        def test_run
           Temjin::ConfigCommand.instance_variable_set(:@config_file_path, @test_config_path)
+          command_args = []
 
-          expected_config_file_contents = {
+          expected = {
             'username' => 'solid_snake',
             'key' => '0123456789abcdef0123456789abcdef',
             'token' => '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
           }
 
           capture_output do |stdin|
-            stdin.puts('solid_snake')
-            stdin.puts('0123456789abcdef0123456789abcdef')
-            stdin.puts('0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef')
+            stdin.puts(expected['username'])
+            stdin.puts(expected['key'])
+            stdin.puts(expected['token'])
             stdin.rewind
-            @init_command.execute
+            @init_command.run(command_args)
           end
 
-          assert_equal(expected_config_file_contents, YAML.load_file(@test_config_path))
+          assert_equal(expected, YAML.load_file(@test_config_path))
         end
       end
     end
--- test/temjin/config_command/show_command_test.rb
+++ test/temjin/config_command/show_command_test.rb
@@ -6,7 +6,7 @@ module Temjin
 
         def setup
           @test_config_path_proper ||= File.join(File.expand_path(Temjin::Test::HOME_DIR), 'files', 'configs', 'proper_config.yml')
-          @show_command = Temjin::ConfigCommand::ShowCommand.new('')
+          @show_command = Temjin::ConfigCommand::ShowCommand.new('temjin config show')
         end
 
         def teardown
@@ -17,27 +17,29 @@ module Temjin
         def test_show_with_existing_config_file
           Temjin::ConfigCommand.instance_variable_set(:@config_file_path, @test_config_path_proper)
           test_config = YAML.load_file(@test_config_path_proper)
+          command_args = []
 
-          expected_stdout = "key: #{test_config['key']}\n" +
+          expected = "key: #{test_config['key']}\n" +
             "token: #{test_config['token']}\n"
 
             actual_stdout, _ = capture_output do
-              @show_command.execute
+              @show_command.run(command_args)
             end
 
-            assert_equal(expected_stdout, actual_stdout)
+            assert_equal(expected, actual_stdout)
         end
 
         def test_show_without_existing_config_file
           Temjin::ConfigCommand.instance_variable_set(:@config_file_path, File.join(File.expand_path(Temjin::Test::HOME_DIR), 'path', 'that', 'does', 'not', 'exist.yml'))
+          command_args = []
 
-          expected_stdout = "Configuration file not found. Please run `temjin config init`.\n"
+          expected = "Configuration file not found. Please run `temjin config init`.\n"
 
           actual_stdout, _ = capture_output do
-            @show_command.execute
+            @show_command.run(command_args)
           end
 
-          assert_equal(expected_stdout, actual_stdout)
+          assert_equal(expected, actual_stdout)
         end
       end
     end
